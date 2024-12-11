6139b54078c9cb3519bffcaadfa17a3c2d5337fb
Prakash kumar <9101prakashkumar@gmail.com>
2024-12-11 15:48:46 +0530

    added route

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import SearchResult from "./components/SearchResults/SearchResult";
 
-export const BASE_URL = "https://chef-restaurant-server.vercel.app/?vercelToolbarCode=QOl18DVg4GMXWHV/";
+export const BASE_URL = "https://chef-restaurant-server.vercel.app/";
 
 const App = () => {
   const [data, setData] = useState(null);
